8f79cfea992dddb48299967f013c248686afd5e2
liuqiang <joeliu1985@outlook.com>
2021-09-24 10:38:06 +0800

    test

--- java-springboot/src/main/java/com/example/websocketdemo/controller/ChatController.java
+++ java-springboot/src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.example.websocketdemo.controller;
 
 import com.example.websocketdemo.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -12,10 +14,11 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ChatController {
-
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        logger.info("log {}",chatMessage.getContent());
         return chatMessage;
     }
 
